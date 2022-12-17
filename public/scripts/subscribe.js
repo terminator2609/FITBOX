d8bbb7e4a7a9ba642a22b2b7f07bcf709456995e
Radostin Dimitrov <73715258+terminator2609@users.noreply.github.com>
2022-12-17 21:37:47 +0200

    ........

--- public/scripts/subscribe.js
+++ public/scripts/subscribe.js
@@ -28,3 +28,24 @@ closeMmenuButton[0].addEventListener("click", (e) => {
     }
     
 })
+
+
+let checkedRound =  document.getElementsByClassName("checked-round")
+let selectInput = document.getElementsByClassName("select-input")
+
+for(let i = 0; i < checkedRound.length; i++) {
+
+    checkedRound[i].addEventListener("click", (e) => {
+
+        for(let index of checkedRound) {
+            index.style.backgroundColor = ""
+        }
+
+        for(let index of selectInput) {
+            index.checked = false
+        }
+
+        checkedRound[i].style.backgroundColor = "#00ff00"
+        selectInput[i].checked = true
+    })
+}
