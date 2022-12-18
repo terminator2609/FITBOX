e47376b4db61f4fbdc85d23460c4492b2c05eacc
Radostin Dimitrov <73715258+terminator2609@users.noreply.github.com>
2022-12-18 17:43:16 +0200

    .........

---
+++ public/scripts/subscription.js
@@ -0,0 +1,35 @@
+let navMenuIcon = document.getElementsByClassName("nav-menu-icon")
+let navMenu = document.getElementsByClassName("nav-menu")
+let otherElements = document.getElementsByClassName("other-element")
+let closeMmenuButton = document.getElementsByClassName("close-menu-button")
+
+
+navMenuIcon[0].addEventListener("click", (e) => {
+    e.preventDefault()
+
+
+    navMenu[0].style.display = "flex"
+
+    for (let index of otherElements) {
+        index.style.display = "none"
+    }
+
+})
+
+
+closeMmenuButton[0].addEventListener("click", (e) => {
+    e.preventDefault()
+
+
+    navMenu[0].style.display = ""
+
+    for (let index of otherElements) {
+        index.style.display = ""
+    }
+
+})
+
+
+
+
+
